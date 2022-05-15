9313ed8f99a3c2163884eb9537e99df9614a576c
Aleksandra Radovic <aradovic@comtrade.com>
2022-05-15 15:18:11 +0200

    Service unit tests

---
+++ the_shop/UnitTests.Service/OrderServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using Application.Repositories;
+using Application.Services;
+using Autofac.Extras.Moq;
+using Domain.Entities;
+using Infrastructure.Repository;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Repository.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.Service
+{
+    public class OrderServiceTests
+    {
+        [Fact]
+        public void CreateOrder()
+        {
+            using var mock = AutoMock.GetLoose();
+
+            // Arrange
+            var quanity = 5;
+            var inventory = new Inventory() { Id = Guid.NewGuid().ToString(), ArticleCode = "111", Price = 500, Quantity = 10, SupplierId = "1" };
+
+            var orderItems = new List<OrderItem> { new OrderItem { Id = Guid.NewGuid().ToString(), Price = inventory.Price, Quantity = quanity, InventoryId = inventory.Id }};
+
+            var customer = new Customer { Id = Guid.NewGuid().ToString(), FirstName = "CustomerName", LastName = "CustomerSurname", Email = "customer@mail.mail", PhoneNumber = "123-456" };
+
+            var context = mock.Mock<InMemoryDbContext>().Object;
+
+            mock.Mock<ILogger<OrderService>>();
+            mock.Mock<IOrderRepository>().Setup(x => x.Add(It.IsAny<Order>()))
+                .Returns(new Order() { Id = Guid.NewGuid().ToString(), TotalPrice = 120, CreatedAt = DateTime.Now, CustomerId = customer.Id, Items = orderItems });
+
+            mock.Mock<ICustomerRepository>().Setup(x => x.GetById(It.IsAny<string>()))
+                .Returns(customer);
+
+            var orderService = mock.Create<OrderService>();
+
+            // Act
+            var actual = orderService.CreateOrder(orderItems, customer.Id);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.Equal(actual.Items, orderItems);
+            Assert.Equal(actual.Items.Sum(oi => oi.Price * oi.Quantity), inventory.Price * quanity);
+        }
+    }
+}
---
+++ the_shop/UnitTests.Service/ShopServiceTests.cs
@@ -0,0 +1,108 @@
+﻿using Application.Repositories;
+using Application.Services;
+using Autofac.Extras.Moq;
+using Domain.Entities;
+using Infrastructure.Repository;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Repository.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace UnitTests.Service
+{
+    public class ShopServiceTests
+    {
+        [Fact]
+        public void FindArticles()
+        {
+            using var mock = AutoMock.GetLoose();
+
+            // Arrange
+            var articleCode = "123";
+            var maxPrice = 250;
+            var quantity = 10;
+
+            var inventories = new List<Inventory> { new Inventory { Id = Guid.NewGuid().ToString(), Price = 250, Quantity = 1, ArticleCode = articleCode, SupplierId = "1" },
+                                               new Inventory { Id = Guid.NewGuid().ToString(), Price = 500, Quantity = 2, ArticleCode = "111", SupplierId = "1" },
+                                               new Inventory { Id = Guid.NewGuid().ToString(), Price = 110, Quantity = 15, ArticleCode = articleCode, SupplierId = "1" }};
+
+            var context = mock.Mock<InMemoryDbContext>().Object;
+
+            mock.Mock<ILogger<ShopService>>();
+            mock.Mock<IInventoryRepository>().Setup(x => x.GetByArticleCodeWithFilter(It.IsAny<string>(), It.IsAny<Func<Inventory, bool>>()))
+                .Returns(inventories.Where(i => i.ArticleCode == articleCode).ToList());
+
+            var shopService = mock.Create<ShopService>();
+
+            // Act
+            var actual = shopService.FindArticles(articleCode, maxPrice, quantity);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.True(actual.Count == 2);
+            Assert.True(actual.All(a => a.ArticleCode == articleCode));
+        }
+
+        [Fact]
+        public void FindArticles_NotEnoughArticlesOnStock()
+        {
+            using var mock = AutoMock.GetLoose();
+
+            // Arrange
+            var articleCode = "123";
+            var maxPrice = 250;
+            var quantity = 10;
+
+            var inventories = new List<Inventory> { new Inventory { Id = Guid.NewGuid().ToString(), Price = 250, Quantity = 1, ArticleCode = articleCode, SupplierId = "1" },
+                                               new Inventory { Id = Guid.NewGuid().ToString(), Price = 500, Quantity = 2, ArticleCode = "111", SupplierId = "1" },
+                                               new Inventory { Id = Guid.NewGuid().ToString(), Price = 110, Quantity = 5, ArticleCode = articleCode, SupplierId = "1" }};
+
+            var context = mock.Mock<InMemoryDbContext>().Object;
+
+            mock.Mock<ILogger<ShopService>>();
+            mock.Mock<IInventoryRepository>().Setup(x => x.GetByArticleCodeWithFilter(It.IsAny<string>(), It.IsAny<Func<Inventory, bool>>()))
+                .Returns(inventories.Where(i => i.ArticleCode == articleCode).ToList());
+
+            var shopService = mock.Create<ShopService>();
+
+            // Act
+            var actual = shopService.FindArticles(articleCode, maxPrice, quantity);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.True(actual.Count == 0);
+        }
+
+        [Fact]
+        public void SellArticle()
+        {
+            using var mock = AutoMock.GetLoose();
+
+            // Arrange
+            var articleCode = "123";
+            var quantity = 10;
+
+            var inventories = new List<Inventory> { new Inventory { Id = Guid.NewGuid().ToString(), Price = 250, Quantity = 1, ArticleCode = articleCode, SupplierId = "1" },
+                                               new Inventory { Id = Guid.NewGuid().ToString(), Price = 110, Quantity = 15, ArticleCode = articleCode, SupplierId = "1" }};
+
+            var context = mock.Mock<InMemoryDbContext>().Object;
+
+            mock.Mock<ILogger<ShopService>>();
+            mock.Mock<IOrderItemRepository>().Setup(x => x.Add(It.IsAny<OrderItem>()))
+                .Returns(new OrderItem() { Id = Guid.NewGuid().ToString() });
+
+            var shopService = mock.Create<ShopService>();
+
+            // Act
+            var actual = shopService.SellArticle(inventories, quantity);
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.True(actual.Count == 2);
+        }
+    }
+}
